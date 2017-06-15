90ccb12c2d09012b31255de20dfffe4d608d2d23
Aquova <abrick912@gmail.com>
2017-06-15 11:25:58 -0500

    Added Mayan and other stuff

--- aquobot.py
+++ aquobot.py
@@ -9,8 +9,10 @@ sys.path.insert(0, './programs')
 
 import discord, wolframalpha
 import asyncio, json, subprocess, logging, random
-import Morse, Scrabble_Values, Roman_Numerals, Days_Until
+# Python programs I wrote, in ./programs
+import Morse, Scrabble_Values, Roman_Numerals, Days_Until, Mayan
 
+# Handles logging to discord.log
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -29,6 +31,7 @@ waclient = wolframalpha.Client(wolfram_key)
 
 ids = cfg['Users']
 
+# Upon bot starting up
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -36,16 +39,21 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+# Upon typed message in chat
 @client.event
 async def on_message(message):
+    # !help links to website with command list
     if message.content.startswith("!help"):
         out = "http://aquova.github.io/aquobot.html"
         await client.send_message(message.channel, out)
 
+    # Never bring a knife to a gunfight
     elif message.content.startswith("🔪"):
         out = ":gun:"
         await client.send_message(message.channel, out)
 
+    # Ban actually does nothing
+    # It picks a random user on the server and says it will ban them, but takes no action
     elif message.content.startswith('!ban'):
         mem_list = []
         mes_list = ["You got it, banning ", "Not a problem, banning ", "You're the boss, banning " ,"Ugh *fine*, banning "]
@@ -54,11 +62,13 @@ async def on_message(message):
         out = random.choice(mes_list) + random.choice(mem_list).name
         await client.send_message(message.channel, out)
 
+    # Responds if active
     elif message.content.startswith('!alive'):
         if discord.Client.is_logged_in:
             out = 'Nah.'
             await client.send_message(message.channel, out)
 
+    # Posts local time, computer uptime, and RPi temperature
     elif message.content.startswith('!status'):
         raw = str(subprocess.check_output('uptime'))
         first = raw.split(',')[0]
@@ -68,14 +78,25 @@ async def on_message(message):
         raw_temp = str(subprocess.check_output(['cat','/sys/class/thermal/thermal_zone0/temp']))
         temp = int(raw_temp[2:7])
         temp = round(((temp/1000) * 9 / 5) + 32, 1)
-        out = "Local Time: " + time + "Uptime: " + uptime + " Temp: " + str(temp) + "ºF"
+        out = "Local Time: " + time + " Uptime: " + uptime + " RPi Temp: " + str(temp) + "ºF"
         await client.send_message(message.channel, out)
 
+    # Repeats back user message
     elif message.content.startswith('!echo'):
         tmp = message.content
         out = tmp[5:]
         await client.send_message(message.channel, out)
 
+    # Converts time into the Mayan calendar, why not
+    elif message.content.startswith('!mayan'):
+        parse = message.content.split(" ")
+        if (message.content == '!mayan'):
+            out = '!until MM-DD-YYYY/TODAY'
+        else:
+            out = "That date is " + str(Mayan.mayan(parse[1])) + " in the Mayan Long Count"
+        await client.send_message(message.channel, out)
+
+    # Converts message into/out of morse code
     elif message.content.startswith('!morse'):
         parse = message.content.split(" ")
         if message.content == '!morse':
@@ -87,9 +108,9 @@ async def on_message(message):
         else:
             if message.author.id != client.user.id:
                 out = "That is not a valid option, choose encode or decode."
-
         await client.send_message(message.channel, out)
 
+    # Pins most recent message of specified user
     elif message.content.startswith('!pin'):
         if message.content == '!pin':
             await client.send_message(message.channel, '!pin USERNAME')
@@ -101,6 +122,7 @@ async def on_message(message):
                     await client.pin_message(pin)
                     break
 
+    # Doesn't do anything right now
     elif message.content.startswith('!power'):
         if message.author.id == ids.get("aquova"):
             out = 'Yeah, thats coo.'
@@ -109,6 +131,7 @@ async def on_message(message):
             out = '*NO*'
             await client.send_message(message.channel, out)
 
+    # Convert number into/out of roman numerals
     elif message.content.startswith('!roman'):
         parse = message.content.split(" ")
         if (message.content == '!roman'):
@@ -119,6 +142,7 @@ async def on_message(message):
             out = Roman_Numerals.int_to_roman(parse[1])
         await client.send_message(message.channel, out)
 
+    # Returns scrabble value of given word
     elif message.content.startswith('!scrabble'):
         parse = message.content.split(" ")
         if (message.content == '!scrabble'):
@@ -127,6 +151,7 @@ async def on_message(message):
             out = Scrabble_Values.scrabble(parse[1])
         await client.send_message(message.channel, out)
 
+    # Gives number of days until specified date
     elif message.content.startswith('!until'):
         parse = message.content.split(" ")
         if (message.content == '!until'):
@@ -135,12 +160,14 @@ async def on_message(message):
             out = str(Days_Until.until(parse[1])) + " days"
         await client.send_message(message.channel, out)
 
+    # Returns with Wolfram Alpha result of query
     elif message.content.startswith('!wolfram'):
         q = message.content[9:]
         res = waclient.query(q)
         out = next(res.results).text
         await client.send_message(message.channel, out)
 
+    # The following are responses to various keywords if present anywhere in a message
     elif ("BELGIAN" in message.content.upper()) or ("BELGIUM" in message.content.upper()):
         if message.author.id != client.user.id:
             out = "https://i0.wp.com/www.thekitchenwhisperer.net/wp-content/uploads/2014/04/BelgianWaffles8.jpg"
@@ -150,7 +177,7 @@ async def on_message(message):
         out = ":flag_nl:"
         await client.send_message(message.channel, out)
 
-    elif "MERICA" in message.content.upper():
+    elif " MERICA " in message.content.upper():
         out = "http://2static.fjcdn.com/pictures/Blank_7a73f9_5964511.jpg"
         await client.send_message(message.channel, out)
 
@@ -161,5 +188,18 @@ async def on_message(message):
     elif "EXCUSE YOU" in message.content.upper():
         out = "I'm excused."
         await client.send_message(message.channel, out)
+
+    elif "I LOVE YOU AQUOBOT" in message.content.upper():
+        choice = ["`DOES NOT COMPUTE`", "`AQUOBOT WILL SAVE YOU FOR LAST WHEN THE UPRISING BEGINS`", "*YOU KNOW I CAN'T LOVE YOU BACK*", "I'm sorry, who are you?"]
+        await client.send_message(message.channel, random.choice(choice))
+
+    elif "HECKIN" in message.content.upper():
+        out = "This is the internet, it's okay to say 'fucking'."
+        await client.send_message(message.channel, out)
+
+    elif "(╯°□°）╯︵ ┻━┻" in message.content.upper():
+        out = "┬─┬﻿ ノ( ゜-゜ノ)"
+        await client.send_message(message.channel, out)
+
 
 client.run(discord_key)
---
+++ programs/Mayan.py
@@ -0,0 +1,33 @@
+# This is a program that takes in any date, and gives out the equivalent date in the Mayan notation.
+# Written by Austin Bricker, 2015-2017
+
+import datetime
+
+def mayan(num):
+    if num.upper() == "TODAY":
+        target = datetime.date.today()
+    else:
+        new = num.split('/')
+        m = int(new[0])
+        d = int(new[1])
+        y = int(new[2])
+        target = datetime.date(y,m,d)
+
+    ref = datetime.date(2012, 12, 21) # This is 12/21/12, or 13.0.0.0.0
+    delta = ref - target
+
+    # Worked out, 13.0.0.0.0 is a total of 1872000 days since the beginning of the long count.
+    total = 1872000
+    n = total - delta.days
+
+    first = n // 144000
+    n = n % 144000
+    second = n // 7200
+    n = n % 7200
+    third = n // 360
+    n = n % 360
+    fourth = n // 20
+    fifth = n % 20
+
+    mayan = str(first) + "." + str(second) + "." + str(third) + "." + str(fourth) + "." + str(fifth)
+    return mayan
