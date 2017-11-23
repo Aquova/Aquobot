820abb87bd07c4182839d64b5f204bb0cf4903dd
Aquova <abrick912@gmail.com>
2017-11-23 16:16:14 -0600

    ACPC working

--- commands/ACPC.py
+++ commands/ACPC.py
@@ -7,20 +7,19 @@ async def main(client, message):
         try:
             out = userinfo.fetchone()[0]
         except TypeError:
-            out = "!acpc [set] USERNAME"
+            out = "!acpc [SET] USERNAME"
     elif message.content.split(" ")[1].upper() == "SET":
         code = " ".join(message.content.split(" ")[2:])
         params = [message.author.id, code]
         sqlconn.execute("INSERT OR REPLACE INTO acpc (userid, code) VALUES (?, ?)", params)
         out = "Friend code added."
     else:
-        q = " ".join(m.content.split(" ")[1:])
+        q = " ".join(message.content.split(" ")[1:])
         try:
             member = discord.utils.get(message.server.members, name=q)
-            code = sqlconn.execute("SELECT code FROM acpc WHERE username=?", [member.id])
-            out = 'Their code is: ' + birth_month.fetchone()[0]
-        except as e:
-            await client.send_message(message.channel, e)
+            code = sqlconn.execute("SELECT code FROM acpc WHERE userid=?", [member.id,])
+            out = 'Their code is: ' + code.fetchone()[0]
+        except:
             out = "Error: No code for that user (searches are case sensitive)."
     sqlconn.commit()
     sqlconn.close()
